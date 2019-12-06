280bf7aa87ca605293429192095d88582354cb23
Kodjo Afriyie <kodjo.afriyie01@bbc.co.uk>
2019-12-06 12:30:49 +0000

    UGCUPLOAD-1055 added the cloudformation macro

--- README.md
+++ README.md
@@ -1,12 +1,48 @@
-# ugc-database-snapshot-id
+# ugc-rds-macro
 
-This lambda returns the latest database snapshot arn.
+This is an aws cloudformation macro that is used to manipulate the template fragment of type `AWS::RDS::DBInstance`
+
+` `**aws s3 mb s3://bucket-name**
+
+### Lambda
+
+##### Setup
+
+Below are a list of steps you should follow to setup the lambda
+
+| Step                              | Action                                                       |
+| --------------------------------- | ------------------------------------------------------------ |
+| Create the bucket for the lambda. | `aws s3 mb s3://rds-snapshot-id-lambda`                      |
+| build zip file                    | `make build`                                                 |
+| Upload zip file to bucket.        | `aws s3 cp rdsmacroinstance.zip s3://rds-snapshot-id-lambda` |
+
+##### Development
+
+After making code changes run the following command to create a new zip, upload to s3 and update the lambda.
+
+``upload.sh``
+
+##### Lambda Configuration
+
+Below are the list of global environment variables used by the lambda
+
+| Parameter            | Description                                                  | Example                                                      |
+| -------------------- | ------------------------------------------------------------ | ------------------------------------------------------------ |
+| db_instance          | This is the arn to the database, used to fetch the latest snapshot | arn:aws:rds:eu-west-2:546933502184:db:mv-ugc-postgres        |
+| latest_snapshot      | This is used to indicate whether to add the latest snapshot the template. Either True or False | True                                                         |
+| properties_to_add    | A command separated list of items to add to the template, each item should be a wellormed json object. | ```{"BackupRetentionPeriod": {"Ref": "BackupRetentionDays"},{"DBName": { "Ref": "DatabaseName"}}`` |
+| properties_to_remove | a comma seperated list of items to remove.                   | BackupRetentionPeriod, DBName                                |
+| snap_shot_type       | If this value is not set not it will only fetch the manual and  automated snapshots. Refere to this:https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds.html#RDS.Client.describe_db_snapshots | shared                                                       |
 
 
-Please refer to links for the possible values:
 
-https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds.html#RDS.Client.describe_db_snapshots
+### Cloudformation Stack
 
-SnapShotType:
-Currently this value is not set, which results on only manual and  automated snapshots being returned.
+To generate the cloudformation stack.
+
+| Step                          | Action                               |
+| ----------------------------- | ------------------------------------ |
+| Generate virtual env          | `make venv`                          |
+| Activate virutal env          | `source env/bin/activate`            |
+| generate cloudformation stack | `python infrastructure/rds_macro.py` |
 
---
+++ infrastructure/rds_macro.py
@@ -0,0 +1,163 @@
+from troposphere import Retain,Template, Sub, Ref, Parameter, GetAtt, Join, Output, Export, GenericHelperFn
+from troposphere.awslambda import Function, Code, Alias, Environment
+from troposphere import iam
+from troposphere.cloudformation import Macro
+from troposphere.logs import LogGroup, Destination
+from awacs.aws import Action,Allow,Policy,PolicyDocument,Principal,Statement,AWSPrincipal,StringEquals,Condition,Null
+from awacs.sts import AssumeRole
+from troposphere.iam import Role
+
+t = Template(Description="Create Macro For DBInstance manipulator and also the function")
+t.add_version("2010-09-09")
+
+ugc_rds_macro_function_name = "ugc-rds-macro"
+
+t.add_description("Used manipulate the DbInstance Template")
+
+rds_macro_handler = t.add_parameter(Parameter(
+    "LambdaHandler",
+    Type="String",
+    Default="ugc_rds_cr_snapshot_id.handler",
+    Description="The name of the function (within your source code) "
+                "that Lambda calls to start running your code."
+))
+
+memory_size = t.add_parameter(Parameter(
+    "LambdaMemorySize",
+    Type="Number",
+    Default="1024",
+    Description="The amount of memory, in MB, that is allocated to "
+                "your Lambda function.",
+    MinValue="128"
+))
+
+env = t.add_parameter(Parameter(
+    "LambdaEnv",
+    AllowedValues=["int", "test", "live"],
+    Description="The environment the lambda is being deployed to",
+    Type="String",
+))
+
+lambda_execution_time = t.add_parameter(Parameter(
+    "LambdaExectionTime",
+    Type="Number",
+    Default="300",
+    Description="Lambda Execution time"
+))
+
+function_role = t.add_resource(
+    Role(
+        "FunctionRole",
+        Policies=[iam.Policy(
+            PolicyName="UgcMoniorFunctionRolePolicy",
+                            PolicyDocument= Policy(
+                                Statement = [Statement(
+                                        Effect=Allow,
+                                        Action = [ Action("logs", "CreateLogGroup"),
+                                            Action("logs", "CreateLogStream"),
+                                            Action("logs", "PutLogEvents")
+                                        ],
+                                        Resource = ["arn:aws:logs:*:*:*"]
+                                    ),Statement(
+                                        Effect=Allow,
+                                        Action=[Action("rds","DescribeDBSnapshots")],
+                                        Resource=["arn:aws:rds:*:*:*"]
+                                    )])
+                )],
+        AssumeRolePolicyDocument=Policy(
+            Statement=[
+                Statement(
+                    Effect=Allow,
+                    Action=[AssumeRole],
+                    Principal=Principal(
+                        "Service", ["lambda.amazonaws.com"]
+                    )
+                )
+            ]
+        )
+    )
+)
+
+rds_macro_lambda = t.add_resource(
+    Function(
+        "RdsSnapShotLambdaFunction",
+        Code=Code(
+            S3Bucket="rds-snapshot-id-lambda",
+            S3Key="rdsmacroinstance.zip"
+        ),
+        Environment=Environment(Variables={
+            'latest_snapshot': 'true',
+            'property_to_remove': 'BackupRetentionPeriod',
+            'property_to_add':'{"BackupRetentionPeriod": {"Ref": "BackupRetentionDays"}}',
+            'db_instance':''
+            }
+        ),
+        Description="Function used to manipulate the dbinstance template",
+        Handler=Ref(rds_macro_handler),
+        MemorySize=Ref(memory_size),
+        FunctionName=Sub("${LambdaEnv}-%s" % ugc_rds_macro_function_name),
+        Role=GetAtt(function_role, "Arn"),
+        Runtime="python3.7",
+        Timeout=180
+    )
+)
+
+role = Role(
+        "RdsMacroLogRole",
+        AssumeRolePolicyDocument=Policy(
+            Statement=[
+                Statement(
+                    Effect=Allow,
+                    Action=[AssumeRole],
+                    Principal=Principal(
+                        "Service", ["cloudformation.amazonaws.com"]
+                    )
+                )
+            ]
+        ),
+        Policies=[iam.Policy(
+                    PolicyName = "FunctionRolePolicy",
+                    PolicyDocument = Policy(
+                        Statement = [ 
+                            Statement(
+                                Effect=Allow,
+                                Action = [ Action("logs", "*")],
+                                Resource = ["arn:aws:logs:*:*:*"]
+                            ),
+                            Statement(
+                                Effect=Allow,
+                                Action=[
+                                    Action("rds","DescribeDBSnapshots")
+                                ],
+                                Resource=["arn:aws:rds:*:*:*"]
+                            ),]
+        ))]
+    )
+
+
+lg = LogGroup(
+    "LogGroupForRDSMacro",
+    DeletionPolicy=Retain
+)
+
+t.add_resource(lg)
+t.add_resource(role)
+
+log_destination = Destination(
+            'MyLogDestination',
+            DestinationName='destination-name',
+            RoleArn='role-arn',
+            TargetArn='target-arn',
+            DestinationPolicy='destination-policy'
+        )
+
+
+dbInstanceMacro = Macro(title="DBInstanceManipulator",
+template=t, validation=False, 
+Description="This is use to invoke the function that modifies the db instance template",
+FunctionName=GetAtt(rds_macro_lambda,"Arn"),
+LogGroupName=Ref(lg),
+LogRoleARN=GetAtt(role, "Arn"),
+Name="UgcRdsMacro")
+
+print(t.to_json())
